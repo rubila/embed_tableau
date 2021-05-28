45a474d34a7e553c3870c03274777fc0d8a4a132
rubila <rubila.adawiyah@tokopedia.com>
2021-05-28 08:16:03 +0700

    change title of the page

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     
-    return render_template('index.html', title='Home')
+    return render_template('index.html', title='The Cost of 1 GB Mobile Data')
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
